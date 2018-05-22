b81ca2cbd51fda4d34a3e0b7da660e9dd04078e7
Victor <artoboi.by@gmail.com>
2018-05-22 15:54:38 +0300

    HTML5Boilerplate

--- public/index.php
+++ public/index.php
@@ -2,14 +2,50 @@
 require_once '../vendor/autoload.php';
 use Medoo\Medoo;
 
-echo "Hello World";
-//$array = [1, "apple", 2, "foo", "bar"];
-//var_dump($array);
-//dump($array);
-
 $database = new Medoo([
     'database_type' => 'sqlite',
-    'database_file' => '../storage/database.db'
+'database_file' => '../storage/database.db'
 ]);
+?>
+<!doctype html>
+<html class="no-js" lang="">
 
-dump($database);
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title></title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <link rel="manifest" href="site.webmanifest">
+  <link rel="apple-touch-icon" href="icon.png">
+  <!-- Place favicon.ico in the root directory -->
+
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+  <!--[if lte IE 9]>
+    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+  <![endif]-->
+
+
+  <!-- Add your site or application content here -->
+  <form method="post">
+      <label>Name: <input type="text" name="name" placeholder="Your name"></label>
+      <label>Email: <input type="text" name="email" placeholder="your@email.com"></label>
+      <label>Comment: <textarea name="comment" cols="30" rows="10"></textarea></label>
+      <input type="submit" value="Save">
+  </form>
+
+
+
+  <script src="js/vendor/modernizr-3.6.0.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+  <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
+  <script src="js/plugins.js"></script>
+  <script src="js/main.js"></script>
+</body>
+
+</html>
